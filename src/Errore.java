dca4c6cc9083072c351f84ddbd4e7556843bc5cd
Tommaso <tommaso.lico@gmail.com>
2026-02-20 13:06:32 +0100

    Classe Errore
    2 Pritnln

---
+++ src/Errore.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Errore {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Errore 21 14");
+        System.out.println("Riguarda le parti semplici");
+    }
+}
