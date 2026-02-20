138a18b3e89aa4aa9ecf91de17c17af57852ad3b
Tommaso <tommaso.lico@gmail.com>
2026-02-20 13:02:14 +0100

    Prova Hello World da Java PC Tommy a GitHub
    Versione 2 Aggiornamento 2 programmi

---
+++ src/Program2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Program2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Ciauuu, prova 21");
+        int a;
+
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextInt();
+
+        System.out.println("Valore"+a);
+
+        scanner.close();
+    }
+
+}
